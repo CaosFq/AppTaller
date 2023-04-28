09b466e752600cd08c018f0acb00f6dba422db9a
CaosFq <andresromerofq@gmail.com>
2023-04-28 16:31:08 +0200

    actSeven

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,14 +1,28 @@
 const User = require('../models/users.models');
+const catchAsync = require('./../utils/catchAsync');
+const bcrypt = require('bcryptjs');
+const generateJWT = require('../utils/jwt');
 
-exports.signup = async (req, res, next) => {
+exports.signup = catchAsync(async (req, res, next) => {
+  const { name, email, password } = req.body;
 
-    const { name, email, password } = req.body;
+  const salt = await bcrypt.genSalt(10);
+  const encryptedPassword = await bcrypt.hash(password, salt);
 
-    const user = await User.create({ name, email, password });
-
-    res.status(201).json({
-      status: 'success',
-      message: 'The user has ben created successfuly!',
-    });
-
-};
+  const user = await User.create({
+    name,
+    email,
+    password: encryptedPassword,
+  });
+  const token = await generateJWT(user.id);
+  res.status(201).json({
+    status: 'success',
+    message: 'The user has ben created successfuly!',
+    token,
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    },
+  });
+});
